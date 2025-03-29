6f8cbec6ff0350bfae99732de6bddfcc5d81b8eb
ailinanto1 <ailin20.uni@gmail.com>
2025-03-29 12:22:37 -0300

    subiendo archivos para el tp1(formulario)

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Formulario from './components/Formulario.vue'; 
+</script>
+
+<template>
+  <h1>Registro de Usuario</h1>
+  <Formulario />
+</template>
+
+<style scoped>
+</style>
